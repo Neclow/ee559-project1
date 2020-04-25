62bca6fd7fa4440704db7928b9789ada81a3ace6
Neclow <neil.scheidwasser-clow@epfl.ch>
2020-04-26 00:02:07 +0200

    Cleaning run

--- test.py
+++ test.py
@@ -2,30 +2,70 @@ import torch
 from torch import nn
 import matplotlib.pyplot as plt
 import time
+import warnings
 from models import *
-from train import train
+from train import train, trial
 from utils import load_data, weight_initialization, train_visualization
+from metrics import compute_accuracy
 
-seed = 14
-# Generate data
-torch.manual_seed(seed)
-train_loader, test_loader = load_data(seed=seed)
+def run_train(model, alpha, alpha_decay, plotting=False, verbose=True, seed=14):
+    # Generate data
+    torch.manual_seed(seed) # For reproducbility
+    train_loader, test_loader = load_data(seed=seed)
 
-best_model = SiameseCNN(mode='dropout')
-best_alpha = 1
-best_alpha_decay = 1
+    # Apply training mode and weight initialization
+    model.train()
+    model.apply(weight_initialization)
 
-# Apply training mode and weight initialization
-best_model.train()
-best_model.apply(weight_initialization)
+    # Train model
+    start = time.time()
+    tr_loss, tr_acc, te_acc = train(model, train_loader, test_loader, alpha=alpha,
+                                    alpha_decay=alpha_decay, verbose=verbose, plotting=plotting)
+    print('Training ended. Training time: %.2f s \n' % (time.time()-start))
 
-# Train
-start = time.time()
-tr_loss, tr_acc, te_acc = train(best_model, train_loader, test_loader, alpha=best_alpha, 
-                                alpha_decay=best_alpha_decay, verbose=True, plotting=True)
-print('Training ended. Training time: %.2f s' % (time.time()-start))
+    # Visualize data if plotting
+    # Else, compute final train and test accuracy
+    if plotting:
+        final_train_accuracy = tr_acc[-1]
+        final_test_accuracy = te_acc[-1]
+        train_visualization(best_model, tr_loss, tr_acc, te_acc)
+    else:
+        final_train_accuracy = compute_accuracy(model, train_loader)
+        final_test_accuracy = compute_accuracy(model, test_loader)
 
-print()
-print('Train accuracy: %.4f. Test accuracy: %.4f' % (tr_acc[-1], te_acc[-1]))
+    print('Train accuracy: %.4f // Test accuracy: %.4f' %
+         (final_train_accuracy, final_test_accuracy))
 
-train_visualization(best_model, tr_loss, tr_acc, te_acc)
+def run(model, alpha, alpha_decay, mode='train', plotting=False, n_trials=10):
+    print('Default run: single training with best model')
+    print('In default run: for plotting, change flag "plotting" to True.')
+    print('For full trial, select mode="trial". Default number of trials: 10.')
+
+    print(f'Model: {model._get_name()}')
+
+    if str.find(model._get_name(), 'Siamese') < 0:
+        warnings.warn(f'Auxiliary loss is not implemented for model: {model._get_name()}', stacklevel=2)
+
+    print(f'Auxiliary loss coefficient: {alpha}')
+    print(f'Auxiliary loss coefficient decay per epoch: {alpha_decay}')
+
+    time.sleep(3)
+
+    if mode == 'train':
+        print('Running: single training.')
+        time.sleep(2)
+        run_train(model, alpha, alpha_decay, plotting=plotting)
+    elif mode == "trial":
+        print(f'Running: {n_trials}-round trial.')
+        trial(model, alpha, alpha_decay, n_trials=n_trials)
+    else:
+        raise ValueError('Running mode not found. Try "train" for simple train, "trial" for full trial.')
+
+def main():
+    best_model = SiameseCNN(mode='dropout', verbose=False)
+    best_alpha = 1
+    best_alpha_decay = 1
+    run(model=best_model, alpha=best_alpha, alpha_decay=best_alpha_decay, mode='train')
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from torch.optim import lr_scheduler
 from utils import load_data, weight_initialization
 from metrics import compute_accuracy
 
-def train(net, train_loader, test_loader=None, eta=1e-3, decay=1e-5, 
+def train(net, train_loader, test_loader=None, eta=1e-3, decay=1e-5,
           n_epochs=25, alpha=1, alpha_decay=1, verbose=False, plotting=False):
     aux_crit = nn.CrossEntropyLoss()
     binary_crit = nn.BCELoss()
@@ -15,21 +15,21 @@ def train(net, train_loader, test_loader=None, eta=1e-3, decay=1e-5,
     tr_losses = torch.zeros(n_epochs)
     tr_accuracies = torch.zeros(n_epochs)
     te_accuracies = torch.zeros(n_epochs)
-    
+
     for e in range(n_epochs):
         # Reset training/validation loss
-        tr_loss, val_loss = 0, 0
-        
+        tr_loss = 0
+
         # Training mode
         net.train()
-        
+
         for (trainX, trainY, trainC) in train_loader:
             # Forward pass
             out, aux = net(trainX)
-            
+
             # Binary classification loss
             binary_loss = binary_crit(out, trainY.float())
-            
+
             # Separate outputs and target classes for each image
             if aux is not None:
                 aux1, aux2 = aux.unbind(1)
@@ -37,39 +37,36 @@ def train(net, train_loader, test_loader=None, eta=1e-3, decay=1e-5,
 
                 # Auxiliary loss
                 aux_loss = aux_crit(aux1, c1) + aux_crit(aux2, c2)
-                
-                # Update total loss
-                total_loss = binary_loss + alpha*aux_loss
             else:
                 # Total loss
-                total_loss = binary_loss
-            
+                aux_loss = 0
+            total_loss = binary_loss + aux_loss
             tr_loss += total_loss.item()
-           
+
             # Backward pass
             optimizer.zero_grad()
             total_loss.backward()
             optimizer.step()
-        
+
         # Collect accuracy data for later plotting
         if plotting:
             tr_accuracies[e] = compute_accuracy(net, train_loader)
             te_accuracies[e] = compute_accuracy(net, test_loader)
-        
+
         # Collect loss data
         tr_losses[e] = tr_loss
-        
+
         # Update auxiliary loss coefficient
         alpha *= alpha_decay
-        
+
         if verbose:
-            print('Epoch %d/%d, Binary loss: %.3f, Auxiliary loss: %.3f' % 
+            print('Epoch %d/%d, Binary loss: %.3f, Auxiliary loss: %.3f' %
                   (e+1, n_epochs, binary_loss, aux_loss))
-        
+
     return tr_losses, tr_accuracies, te_accuracies
 
 
-def trial(net, n_epochs=25, n_trials=30, alpha=0, alpha_decay=1, verbose=False):
+def trial(net, n_trials=30, n_epochs=25, alpha=0, alpha_decay=1, verbose=False):
     all_losses = torch.zeros((n_trials, n_epochs))
     tr_accuracies = torch.zeros(n_trials)
     te_accuracies = torch.zeros(n_trials)
@@ -77,33 +74,33 @@ def trial(net, n_epochs=25, n_trials=30, alpha=0, alpha_decay=1, verbose=False):
         # Shuffle data
         torch.manual_seed(i)
         train_loader, test_loader = load_data(seed=i)
-        
+
         # Reset weights
         net.train()
         net.apply(weight_initialization)
-        
+
         # Train
         start = time.time()
-        tr_loss = train(net, train_loader, alpha=alpha, alpha_decay=alpha_decay)[0]
+        tr_loss = train(net, train_loader, n_epochs=n_epochs, alpha=alpha, alpha_decay=alpha_decay)[0]
         print('Trial %d/%d... Training time: %.2f s' % (i+1, n_trials, time.time()-start))
-        
+
         # Collect data
         all_losses[i] = tr_loss
-        
+
         # Compute train and test accuracy
         net.eval() # Dropout layers will work in eval mode
         with torch.no_grad():
             tr_accuracies[i] = compute_accuracy(net, train_loader)
             te_accuracies[i] = compute_accuracy(net, test_loader)
-        
+
         if verbose:
-            print('Loss: %.4f, Train acc: %.4f, Test acc: %.4f' % 
+            print('Loss: %.4f, Train acc: %.4f, Test acc: %.4f' %
                   (train_loss[-1], tr_accuracies[i], te_accuracies[i]))
-            
-    # Print trial results        
+
+    # Print trial results
     print('Train accuracy - mean: %.4f, std: %.4f, median: %.4f' %
          (tr_accuracies.mean(), tr_accuracies.std(), tr_accuracies.median()))
-    print('Test accuracy - mean: %.4f, std: %.4f, median: %.4f' % 
+    print('Test accuracy - mean: %.4f, std: %.4f, median: %.4f' %
          (te_accuracies.mean(), te_accuracies.std(), te_accuracies.median()))
-    
+
     return all_losses, tr_accuracies, te_accuracies
